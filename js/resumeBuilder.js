af61c3959870ffcbde40497aeb81bbdb043c8dc2
jean-michel-legrand <defactojml@gmail.com>
2015-08-04 19:41:33 +0200

    Before q&a session

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -5,17 +5,16 @@ var bio = {
   "name": "Jean-Michel Legrand",
   "role": "Front Engineer",
   "contacts": {
-    "mobile": "33-6-6-82-26-26-65",
+    "mobile": "33 6 82 26 26 65",
     "email": "jml.legrand@gmail.com",
-    "twitter": "jmllegrand",
-    "github": "jmllegrand",
+    "twitter": "@jmllegrand",
+    "github": "defactojml",
     "location": "Paris"
   },
 
   "welcomeMessage": "First heard about Agile in US in 2001 while the manifesto was written. " +
-  " Since then, Jean-Michel has been working on a number of high business profile projects" +
-  " 2 main business context: Insurance and Clearing & Custody " +
-    "Positions held: From COach to Scrum Master to Developper",
+  " Since then, Jean-Michel has been working on a number of high business profile projects <br>" +
+  " 2 main business context: Insurance and Clearing & Custody. <br> ",
   "skills": ["Team Leadership, Team Player", "Lean / Agile values", "Scrum, XP & Kanban", "JS", "Spring MVC", "SVN, Jira, Jenkins"],
   "biopic": "images/fry.jpg",
   "display": function () {
@@ -26,6 +25,8 @@ var bio = {
     // use $.each() to not only iterate over arrays but over objets as well
     $.each(bio.contacts, function (property, value) {
       $("#topContacts").append(HTMLcontactGeneric.replace("%contact%", property).replace("%data%", value));
+      $("#footerContacts").append(HTMLcontactGeneric.replace("%contact%", property).replace("%data%", value));
+
     });
 
     $("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
@@ -80,13 +81,13 @@ var projects = {
     {
       title: "new sesame",
       dates: "2014-present",
-      description: "Reengineering of the sales tools for bank advisor - Target 2 major retail banking actors in France",
+      description: "Reengineering of the sales tools for bank tellers selling insurance products - Target 2 major retail banking actors in France",
       images: []
     },
     {
       title: "acetp",
       dates: "2002-2005",
-      description: "reengineering reengineeringreengineeringreengineeringreengineeringreengineeringreengineeringreengineering",
+      description: "reengineering",
       images: []
     }
   ],
